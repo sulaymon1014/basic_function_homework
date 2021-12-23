3e9fd62ed034a65313062d10aac4fd76122d0227
sulaymon1014 <sulaymonruziboev75@gmail.com>
2021-12-23 20:44:34 +0500

    3

--- basic_func03.py
+++ basic_func03.py
@@ -1,3 +1,8 @@
 # Create a function called main.
 
 # Return the value "codeschooluz"
+
+def main():
+    return "codeschooluz"  
+main()
+print(main()) 
--- basic_func04.py
+++ basic_func04.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 
 # Return the value integer type.
+
+def main():
+    return 12
+main()
+
+print(main())
